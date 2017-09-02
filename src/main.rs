0e66eaf826be2e3ffe48925ca7f1d1fce970fdac
Nicholas Harring <nharring@gmail.com>
2017-09-02 07:58:48 -0700

    Codegen

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,6 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use thrift::protocol::{TInputProtocolFactory, TOutputProtocolFactory};
 use thrift::protocol::{TCompactInputProtocolFactory, TCompactOutputProtocolFactory};
-use thrift::server::TSimpleServer;
-
-use thrift::transport::{TFramedTransportFactory, TTransportFactory};
-
 use kv_server::{KeyNotFound, ServiceException};
 use kv_server::{KVObject};
 
